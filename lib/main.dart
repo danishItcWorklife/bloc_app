4035092493e9ea32ab9079dd0a710793d2a709f8
danish <danishitcworklife@gmail.com>
2024-03-14 15:41:59 +0000

    Implemented Hive in Bloc with offline online support

--- lib/main.dart
+++ lib/main.dart
@@ -8,10 +8,20 @@ import 'package:hive_flutter/hive_flutter.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Hive.initFlutter();
+  //////////////Without Encryption////////////
+  // await Hive.initFlutter();
+  // Hive.registerAdapter(ProductListEntityAdapter());
+  // Hive.registerAdapter(ProductListProductsAdapter());
+  // final postsBox = await Hive.openBox<ProductListEntity>(MyConstants.boxProductsListKey);
+  // await Hive.initFlutter();
+
+  //////////////With Encryption////////////
+  final encryptionKey = Hive.generateSecureKey();
   Hive.registerAdapter(ProductListEntityAdapter());
   Hive.registerAdapter(ProductListProductsAdapter());
-  final postsBox =
-      await Hive.openBox<ProductListEntity>(MyConstants.boxProductsListKey);
+  final postsBox = await Hive.openBox<ProductListEntity>(
+      MyConstants.boxProductsListKey,
+      encryptionCipher: HiveAesCipher(encryptionKey));
+  ////////////////////////////////////
   runApp(HomePage(productRepository: ProductRepository(postsBox: postsBox)));
 }
